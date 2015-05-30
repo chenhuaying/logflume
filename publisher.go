fce38cfeee53c85ba77d2eb3f0fb1da82d2581aa
flying-pig <chying.ren@gmail.com>
2015-05-31 06:36:09 +0800

    add kafka push

--- publisher.go
+++ publisher.go
@@ -1,14 +1,48 @@
 package main
 
 import (
-	_ "github.com/Shopify/sarama"
+	"github.com/Shopify/sarama"
 	"log"
+	"time"
 )
 
 func Publish(input chan *FileEvent, registrar chan *FileEvent) {
 	log.Println("publish loop")
+
+	clientConfig := sarama.NewConfig()
+	clientConfig.Producer.RequiredAcks = sarama.WaitForLocal
+	clientConfig.Producer.Compression = sarama.CompressionSnappy
+	clientConfig.Producer.Flush.Frequency = 500 * time.Millisecond
+	clientConfig.Producer.Return.Successes = true
+
+	brokerList := []string{"127.0.0.1:9092"}
+	producer, err := sarama.NewAsyncProducer(brokerList, clientConfig)
+	if err != nil {
+		log.Fatalln("Failed to start Sarama producer: ", err)
+	}
+
+	go func() {
+		for {
+			select {
+			case error := <-producer.Errors():
+				log.Println("error:", error)
+			case success := <-producer.Successes():
+				log.Println("OK:", success,
+					success.Metadata.(*FileEvent).Offset,
+					*success.Metadata.(*FileEvent).Source)
+			}
+		}
+	}()
+
 	for event := range input {
 		log.Println(*event.Text)
+		producer.Input() <- &sarama.ProducerMessage{
+			Topic:    "test",
+			Key:      sarama.StringEncoder("test-key"),
+			Value:    sarama.StringEncoder(*event.Text),
+			Metadata: event,
+		}
 		registrar <- event
 	}
+
 }
